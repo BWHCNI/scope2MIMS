c8e7c933bb42c6ed76d19885af8725237b84900a
Boris Epstein <borepstein@gmail.com>
2009-02-26 16:17:44 +0000

    Routine check-in. Minor code/comment additions.
    git-svn-id: https://zoomwhee.mgh.harvard.edu/svn_devel/mims_proc_utils/trunk/holder_ref_data@9 3c3b203a-996e-4079-9301-5f6b36899a1c

--- src/holder_data_mgmt/RefPointList.java
+++ src/holder_data_mgmt/RefPointList.java
@@ -1,13 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package holder_data_mgmt;
 
 /**
  *
  * @author bepstein
+ * The list of reference points. This class mimics the original representation fo the holder coordinates data file.
  */
 public class RefPointList {
     /* constructors */
