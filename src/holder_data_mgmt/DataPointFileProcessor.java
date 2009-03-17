4f152d27fcbace6fec8b96ebae7ef89015eed4c5
Boris Epstein <borepstein@gmail.com>
2009-03-17 19:58:01 +0000

    Routine check-in.
    git-svn-id: https://zoomwhee.mgh.harvard.edu/svn_devel/mims_proc_utils/trunk/holder_ref_data@24 3c3b203a-996e-4079-9301-5f6b36899a1c

---
+++ src/holder_data_mgmt/DataPointFileProcessor.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package holder_data_mgmt;
+
+/**
+ * The class intended to run a transformation of the data points file into the .ref format.
+ * @author bepstein
+ */
+public class DataPointFileProcessor {
+
+
+}
