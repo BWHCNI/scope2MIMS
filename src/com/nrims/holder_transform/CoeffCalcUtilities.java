64c0440191984a061c0782ad1e162a017b6bf8ee
Boris Epstein <borepstein@gmail.com>
2009-05-21 19:10:58 +0000

    Introducuing the new utilities file for coeff. calculations.
    git-svn-id: https://zoomwhee.mgh.harvard.edu/svn_devel/mims_proc_utils/trunk/holder_ref_data@65 3c3b203a-996e-4079-9301-5f6b36899a1c

---
+++ src/com/nrims/holder_transform/CoeffCalcUtilities.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.nrims.holder_transform;
+
+/**
+ *
+ * @author bepstein
+ */
+
+/* This is a utility class for calculation logic
+ * used in calculating coefficients for coordinate translation.
+ */
+public class CoeffCalcUtilities {
+
+}
